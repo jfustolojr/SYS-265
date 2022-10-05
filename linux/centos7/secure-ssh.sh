3256237316301e177ebd9fd71f96324d63248411
jfustolojr <jfustolojr@gmail.com>
2022-10-05 05:17:02 +0000

    Final script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -5,20 +5,25 @@
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
 
-user = ''
+declare newuser
 
-while getopts 'user' flag; do
-	case "${flag}" in
-		user} user="${OPTARG}" ;;
-	esac
+while getopts 'u:' flag; do
+  case $flag in
+    u) new_user=$OPTARG
+    ;;
+  esac
 done
 
-sudo useradd -m -d /home/$user -s /bin/bash $user
-sudo mkdir /home/$user/.ssh
-sudo cp ../public-keys/id_rsa.pub /home/$user/.ssh/authorized_keys
-sudo chmod 700 /home/$user/.ssh
-sudo chmod 600 /home/$user/.ssh/authorized_keys
-sudo chown -R $user:$user /home/$user/.ssh
+home_dir="/home/${new_user}"
+ssh_dir="${home_dir}/.ssh"
+auth_keys="${ssh_dir}/authorized_keys"
+
+sudo useradd -m -d ${home_dir} -s /bin/bash ${new_user}
+sudo mkdir ${ssh_dir}
+sudo cp ../public-keys/id_rsa.pub ${auth_keys}
+sudo chmod 700 ${home_dir}
+sudo chmod 600 ${auth_keys}
+sudo chown -R ${new_user}:${new_user} ${auth_keys}
 
 sudo echo "PermitRootLogin no" >> /etc/ssh/sshd_config
 sudo systemctl restart sshd.service
